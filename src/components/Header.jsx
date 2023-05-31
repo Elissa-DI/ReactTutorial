b0ca41ff68d8fb9a30dedb132bd06b2d00efdd60
Elissa <elissadusabe@gmail.com>
2023-05-31 21:42:58 +0200

    new features

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <main>
+        <h3 className='bg-dark d-flex justify-content-center text-white'>Groceries List</h3>
+    </main>
+  )
+}
+
+export default Header
