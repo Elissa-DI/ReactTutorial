2bd3f92778e79dc4799c0061ea375ee0932ca958
Elissa <elissadusabe@gmail.com>
2023-05-31 21:41:54 +0200

    new features

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Content />
+      <Footer className='footer'/>
     </div>
   );
 }
